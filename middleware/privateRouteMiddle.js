3401a291882281faf469ee2f4c29d3a1b7c1e911
Samay Dhawale <samaydhawale1@gmail.com>
2023-07-16 22:17:18 +0530

    done

--- middleware/privateRouteMiddle.js
+++ middleware/privateRouteMiddle.js
@@ -1,12 +1,17 @@
 
-
 const privateRouteMiddle = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  if (token) {
-    next();
-  } else {
-    res.json({ msg: "You are not Logged in please login!!" });
+  if(req.headers.authorization){
+    const token = req.headers.authorization.split(" ")[1];
+    if (token) {
+      next();
+    } else {
+      res.json({ msg: "You are not Logged in please login!!"});
+    }
   }
+  else{
+    res.json({ msg: "You are not Logged in please login!!"});
+  }
+  
 };
 
 module.exports = privateRouteMiddle;
