b0d22783a1d7875020d1775a79e5209300f1226f
Samay Dhawale <samaydhawale1@gmail.com>
2023-07-16 23:39:22 +0530

    done

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express()
 app.use(express.json())
 app.use("/users", userRoute)
 app.use("/posts", postsRoute)
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 
 
